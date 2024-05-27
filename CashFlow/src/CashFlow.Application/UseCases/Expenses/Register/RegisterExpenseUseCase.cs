ebffe300a4322260d499f9266e4592ea90d1d2c1
Gabriel Santos <gabriels.santosx@gmail.com>
2024-05-27 00:03:27 -0300

    Initial commit

---
+++ CashFlow/src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,29 @@
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+using CashFlow.Exception.ExceptionsBase;
+
+namespace CashFlow.Application.UseCases.Expenses.Register;
+
+public class RegisterExpenseUseCase
+{
+    public ResponseRegisterExpenseJson Execute(RequestRegisterExpenseJson request)
+    {
+        Validate(request);
+
+        return new ResponseRegisterExpenseJson();
+    }
+
+    private void Validate(RequestRegisterExpenseJson request)
+    {
+        var validator = new RegisterExpenseValidator();
+
+        var result = validator.Validate(request);
+
+        if (!result.IsValid)
+        {
+            var errorMessages = result.Errors.Select(failure => failure.ErrorMessage).ToList();
+
+            throw new ErrorOnValidationException(errorMessages);
+        }
+    }
+}
